11dd06b629e0f2a8b05d5e860a3614a06102ce80
Abhay <abc@gmail.com>
2022-06-26 15:09:33 +0530

    added some changes

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,30 +7,36 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-      <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light" style={{borderBottom:'1px solid black', height:'55px'}}>
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/">NewsTimes</a>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src="favicon.ico" style={{ height: '45px', width: '45px' }} />
+          <h1 className="mx-2">NewsTimes</h1>
+        </div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" style={{ borderBottom: '1px solid black', height: '60px' }}>
+          <div className="container-fluid">
+            <img src="favicon.ico" style={{ height: '45px', width: '45px' }} />
+
+            <a className="navbar-brand fw-bold mx-2" href="/">NewsTimes</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                 <li className="nav-item "><a className="nav-link active " aria-current="page" href="/"><button type="button" className="btn btn-outline-white">Home</button></a></li>
-                 
+
                 <li className="nav-item" ><a className="nav-link active" href="/business"><button type="button" className="btn btn-outline-white">Business</button></a></li>
                 <li className="nav-item" ><a className="nav-link active" href="/entertainment"><button type="button" className="btn btn-outline-white">Entertainment</button> </a></li>
                 <li className="nav-item" ><a className="nav-link active" href="/general"><button type="button" className="btn btn-outline-white">General</button></a></li>
                 <li className="nav-item" ><a className="nav-link active" href="/health"><button type="button" className="btn btn-outline-white">Health</button> </a></li>
                 <li className="nav-item" ><a className="nav-link active" href="/science"><button type="button" className="btn btn-outline-white">Science</button> </a></li>
                 <li className="nav-item" ><a className="nav-link active" href="/sports"><button type="button" className="btn btn-outline-white">sports</button> </a></li>
-                <li className="nav-item" ><a className="nav-link active" href="/technology"><button type="button" className="btn btn-outline-white">Technology</button> </a></li>  
-                
-            </ul>
-            
+                <li className="nav-item" ><a className="nav-link active" href="/technology"><button type="button" className="btn btn-outline-white">Technology</button> </a></li>
+
+              </ul>
+
             </div>
-        </div>
-    </nav>
-    </div>
+          </div>
+        </nav>
+      </div>
     )
   }
 }
